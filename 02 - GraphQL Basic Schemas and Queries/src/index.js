243bdf146cb15983ca5350ae12cb109ea31dbfd8
ArianAngoma <arian.angoma@tecsup.edu.pe>
2021-12-20 08:42:04 -0500

    GraphQL API creado

--- 02 - GraphQL Basic Schemas and Queries/src/index.js
+++ 02 - GraphQL Basic Schemas and Queries/src/index.js
@@ -1,12 +1,36 @@
-import myCurrentLocation, {getGreeting, message, name} from './myModule';
-import myAddFunction, {subtract} from './math';
+import {GraphQLServer} from 'graphql-yoga';
 
-console.log(message);
-console.log(name);
-console.log(myCurrentLocation);
+/* Type definitions -> Describe las operaciones y estructura de los datos */
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`
 
-console.log(getGreeting('Arian Angoma'));
+/* Resolvers -> Funciones que resuelven las consultas */
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is my first query'
+        },
+        name() {
+            return 'Arian Angoma'
+        },
+        location() {
+            return 'Perú'
+        },
+        bio() {
+            return 'Software Developer'
+        }
+    }
+}
 
-console.log(myAddFunction(1, 2));
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
 
-console.log(subtract(2, 1));
+server.start(() => console.log('The server is up'));
