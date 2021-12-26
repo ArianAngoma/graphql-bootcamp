01f8a7934cb1f88fc05c0e4779ff32ecf05650bc
ArianAngoma <arian.angoma@tecsup.edu.pe>
2021-12-26 12:41:41 -0500

    Eliminar Usuario

--- 02 - GraphQL Basic Schemas and Queries/src/index.js
+++ 02 - GraphQL Basic Schemas and Queries/src/index.js
@@ -2,7 +2,7 @@ const {GraphQLServer} = require('graphql-yoga');
 const {v4: uuidv4} = require('uuid');
 
 /* Demo user data */
-const users = [
+let users = [
     {
         id: '1',
         name: 'Arian',
@@ -22,7 +22,7 @@ const users = [
 ];
 
 /* Demo post data */
-const posts = [
+let posts = [
     {
         id: '1',
         title: 'GraphQL',
@@ -47,7 +47,7 @@ const posts = [
 ]
 
 /* Demo comment data */
-const comments = [
+let comments = [
     {
         id: '1',
         text: 'This worked well for me.',
@@ -87,6 +87,7 @@ const typeDefs = `
     
     type Mutation {
        createUser(data: CreateUserInput!): User!
+       deleteUser(id: ID!): User!
        createPost(data: CreatePostInput!): Post!
        createComment(data: CreateCommentInput!): Comment!
     }
@@ -195,6 +196,25 @@ const resolvers = {
             users.push(user);
 
             return user;
+        },
+        deleteUser(parent, args, ctx, info) {
+            const userIndex = users.findIndex(user => user.id === args.id);
+
+            if (userIndex === -1) throw new Error('User nor found');
+
+            const deletedUsers = users.splice(userIndex, 1);
+
+            posts = posts.filter(post => {
+                const match = post.author === args.id;
+
+                if (match) comments = comments.filter(comment => comment.post !== post.id);
+
+                return !match;
+            });
+
+            comments = comments.filter(comment => comment.author !== args.id);
+
+            return deletedUsers[0];
         },
         createPost(parent, args, ctx, info) {
             const userExists = users.some(user => user.id === args.data.author);
