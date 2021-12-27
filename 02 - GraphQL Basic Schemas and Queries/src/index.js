7db23ac7cbd085f410a16f4057f6d081c731e7c5
ArianAngoma <arian.angoma@tecsup.edu.pe>
2021-12-26 21:18:45 -0500

    Eliminar Comentario

--- 02 - GraphQL Basic Schemas and Queries/src/index.js
+++ 02 - GraphQL Basic Schemas and Queries/src/index.js
@@ -91,6 +91,7 @@ const typeDefs = `
        createPost(data: CreatePostInput!): Post!
        deletePost(id: ID!): Post!
        createComment(data: CreateCommentInput!): Comment!
+       deleteComment(id: ID!): Comment!
     }
     
     input CreateUserInput {
@@ -236,11 +237,11 @@ const resolvers = {
 
             if (postIndex === -1) throw new Error('Post not found');
 
-            const deletePosts = posts.splice(postIndex, 1);
+            const deletedPosts = posts.splice(postIndex, 1);
 
             comments = comments.filter(comment => comment.post !== args.id);
 
-            return deletePosts[0];
+            return deletedPosts[0];
         },
         createComment(parent, args, ctx, info) {
             const userExists = users.some(user => user.id === args.data.author);
@@ -256,6 +257,15 @@ const resolvers = {
             comments.push(comment);
 
             return comment;
+        },
+        deleteComment(parent, args, ctx, info) {
+            const commentIndex = comments.findIndex(comment => comment.id === args.id);
+
+            if (commentIndex === -1) throw new Error('Comment not found');
+
+            const deletedComments = comments.splice(commentIndex, 1);
+
+            return deletedComments[0];
         }
     },
     Post: {
