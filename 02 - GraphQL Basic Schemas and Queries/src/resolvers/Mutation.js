1b997a74322ef8e3904d8f4db3f9ce11ec934d25
ArianAngoma <arian.angoma@tecsup.edu.pe>
2021-12-29 09:40:34 -0500

    Subscription Post

--- 02 - GraphQL Basic Schemas and Queries/src/resolvers/Mutation.js
+++ 02 - GraphQL Basic Schemas and Queries/src/resolvers/Mutation.js
@@ -54,7 +54,7 @@ const Mutation = {
 
         return user;
     },
-    createPost(parent, args, {db}, info) {
+    createPost(parent, args, {db, pubsub}, info) {
         const userExists = db.users.some(user => user.id === args.data.author);
 
         if (!userExists) throw new Error('User nor found');
@@ -65,6 +65,8 @@ const Mutation = {
         }
 
         db.posts.push(post);
+
+        if (args.data.published) pubsub.publish('post', {post});
 
         return post;
     },
