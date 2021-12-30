cfba4580c94dcdff05846001ab104d58bba82720
ArianAngoma <arian.angoma@tecsup.edu.pe>
2021-12-30 10:53:06 -0500

    Subscription Post

--- 02 - GraphQL Basic Schemas and Queries/src/resolvers/Mutation.js
+++ 02 - GraphQL Basic Schemas and Queries/src/resolvers/Mutation.js
@@ -66,23 +66,37 @@ const Mutation = {
 
         db.posts.push(post);
 
-        if (args.data.published) pubsub.publish('post', {post});
+        if (args.data.published) pubsub.publish('post', {
+            post: {
+                mutation: 'CREATED',
+                data: post
+            }
+        });
 
         return post;
     },
-    deletePost(parent, args, {db}, info) {
+    deletePost(parent, args, {db, pubsub}, info) {
         const postIndex = db.posts.findIndex(post => post.id === args.id);
 
         if (postIndex === -1) throw new Error('Post not found');
 
-        const deletedPosts = db.posts.splice(postIndex, 1);
+        const [post] = db.posts.splice(postIndex, 1);
 
         db.comments = db.comments.filter(comment => comment.post !== args.id);
 
-        return deletedPosts[0];
+        if (post.published) pubsub.publish('post', {
+            post: {
+                mutation: 'DELETED',
+                data: post
+            }
+        });
+
+        return post;
     },
-    updatePost(parent, {id, data}, {db}, info) {
+    updatePost(parent, {id, data}, {db, pubsub}, info) {
         const post = db.posts.find(post => post.id === id);
+
+        const originalPost = {...post};
 
         if (!post) throw new Error('Post not found');
 
@@ -90,7 +104,42 @@ const Mutation = {
 
         if (typeof data.body === 'string') post.body = data.body;
 
-        if (typeof data.published === 'boolean') post.published = data.published;
+        if (typeof data.published === 'boolean') {
+            post.published = data.published;
+
+            if (originalPost.published && !post.published) {
+                /* Deleted */
+                pubsub.publish('post', {
+                    post: {
+                        mutation: 'DELETED',
+                        data: originalPost
+                    }
+                });
+            } else if (!originalPost.published && post.published) {
+                /* Created */
+                pubsub.publish('post', {
+                    post: {
+                        mutation: 'CREATED',
+                        data: post
+                    }
+                });
+            } else if (originalPost.published && post.published) {
+                pubsub.publish('post', {
+                    post: {
+                        mutation: 'UPDATED',
+                        data: post
+                    }
+                });
+            }
+        } else if (post.published) {
+            /* Updated */
+            pubsub.publish('post', {
+                post: {
+                    mutation: 'UPDATED',
+                    data: post
+                }
+            });
+        }
 
         return post;
     },
